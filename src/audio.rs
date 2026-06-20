3cba727147425f9aac61df4e13e2020efef04b6f
praveensaummya <praveensaummya25@gmail.com>
2026-06-20 23:43:41 +0530

    Initial commit: RustMusic cross-platform music player

---
+++ src/audio.rs
@@ -0,0 +1,239 @@
+use rodio::{Decoder, OutputStream, Sink, Source};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+pub enum AudioCommand {
+    Play(PathBuf),
+    Pause,
+    Resume,
+    Stop,
+    Seek(f64),
+    SetVolume(f32),
+    Quit,
+}
+
+pub enum AudioStatus {
+    Playing,
+    Paused,
+    Stopped,
+    Finished,
+    Error(String),
+}
+
+pub struct AudioEngine {
+    command_sender: Sender<AudioCommand>,
+    status_receiver: Receiver<AudioStatus>,
+    current_position: Arc<Mutex<f64>>,
+    total_duration: Arc<Mutex<f64>>,
+    current_song: Arc<Mutex<Option<String>>>,
+}
+
+impl AudioEngine {
+    pub fn new() -> Self {
+        let (cmd_tx, cmd_rx) = mpsc::channel::<AudioCommand>();
+        let (status_tx, status_rx) = mpsc::channel::<AudioStatus>();
+        let current_position = Arc::new(Mutex::new(0.0));
+        let total_duration = Arc::new(Mutex::new(0.0));
+        let current_song = Arc::new(Mutex::new(None));
+
+        let pos = current_position.clone();
+        let dur = total_duration.clone();
+        let song = current_song.clone();
+
+        thread::spawn(move || {
+            let (_stream, stream_handle) = match OutputStream::try_default() {
+                Ok(s) => s,
+                Err(e) => {
+                    let _ = status_tx.send(AudioStatus::Error(format!(
+                        "Failed to open audio output: {}",
+                        e
+                    )));
+                    return;
+                }
+            };
+
+            let mut sink: Option<Sink> = None;
+            let mut is_paused = false;
+            let mut volume = 1.0;
+
+            loop {
+                // Update position if playing
+                if let Some(ref s) = sink {
+                    if !s.empty() && !is_paused {
+                        if let Ok(mut p) = pos.lock() {
+                            *p = s.get_pos().as_secs_f64();
+                        }
+                    }
+                    if s.empty() && !is_paused && sink.is_some() {
+                        let _ = status_tx.send(AudioStatus::Finished);
+                        if let Ok(mut p) = pos.lock() {
+                            *p = 0.0;
+                        }
+                        sink = None;
+                        if let Ok(mut s) = song.lock() {
+                            *s = None;
+                        }
+                    }
+                }
+
+                // Check for commands
+                match cmd_rx.try_recv() {
+                    Ok(AudioCommand::Play(path)) => {
+                        // Stop current playback
+                        if let Some(s) = sink.take() {
+                            s.stop();
+                        }
+                        is_paused = false;
+
+                        // Try to play the new file
+                        match File::open(&path) {
+                            Ok(file) => {
+                                let reader = BufReader::new(file);
+                                match Decoder::new(reader) {
+                                    Ok(source) => {
+                                        let total = source.total_duration();
+                                        if let Ok(mut d) = dur.lock() {
+                                            *d = total.map(|t| t.as_secs_f64()).unwrap_or(0.0);
+                                        }
+                                        if let Ok(mut p) = pos.lock() {
+                                            *p = 0.0;
+                                        }
+                                        if let Ok(mut s) = song.lock() {
+                                            *s = path
+                                                .file_name()
+                                                .map(|n| n.to_string_lossy().to_string());
+                                        }
+
+                                        let new_sink = Sink::try_new(&stream_handle).unwrap();
+                                        new_sink.set_volume(volume);
+                                        new_sink.append(source);
+                                        sink = Some(new_sink);
+                                        let _ = status_tx.send(AudioStatus::Playing);
+                                    }
+                                    Err(e) => {
+                                        let _ = status_tx.send(AudioStatus::Error(format!(
+                                            "Failed to decode audio: {}",
+                                            e
+                                        )));
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                let _ = status_tx.send(AudioStatus::Error(format!(
+                                    "Failed to open file: {}",
+                                    e
+                                )));
+                            }
+                        }
+                    }
+                    Ok(AudioCommand::Pause) => {
+                        if let Some(ref s) = sink {
+                            s.pause();
+                            is_paused = true;
+                            let _ = status_tx.send(AudioStatus::Paused);
+                        }
+                    }
+                    Ok(AudioCommand::Resume) => {
+                        if let Some(ref s) = sink {
+                            s.play();
+                            is_paused = false;
+                            let _ = status_tx.send(AudioStatus::Playing);
+                        }
+                    }
+                    Ok(AudioCommand::Stop) => {
+                        if let Some(s) = sink.take() {
+                            s.stop();
+                        }
+                        is_paused = false;
+                        if let Ok(mut p) = pos.lock() {
+                            *p = 0.0;
+                        }
+                        if let Ok(mut s) = song.lock() {
+                            *s = None;
+                        }
+                        let _ = status_tx.send(AudioStatus::Stopped);
+                    }
+                    Ok(AudioCommand::Seek(_pos)) => {
+                        // rodio doesn't support seeking natively
+                        // This is a limitation we'll work around
+                    }
+                    Ok(AudioCommand::SetVolume(v)) => {
+                        volume = v;
+                        if let Some(ref s) = sink {
+                            s.set_volume(v);
+                        }
+                    }
+                    Ok(AudioCommand::Quit) => {
+                        if let Some(s) = sink.take() {
+                            s.stop();
+                        }
+                        break;
+                    }
+                    Err(mpsc::TryRecvError::Empty) => {}
+                    Err(mpsc::TryRecvError::Disconnected) => break,
+                }
+
+                thread::sleep(Duration::from_millis(50));
+            }
+        });
+
+        AudioEngine {
+            command_sender: cmd_tx,
+            status_receiver: status_rx,
+            current_position,
+            total_duration,
+            current_song,
+        }
+    }
+
+    pub fn play(&self, path: PathBuf) {
+        let _ = self.command_sender.send(AudioCommand::Play(path));
+    }
+
+    pub fn pause(&self) {
+        let _ = self.command_sender.send(AudioCommand::Pause);
+    }
+
+    pub fn resume(&self) {
+        let _ = self.command_sender.send(AudioCommand::Resume);
+    }
+
+    pub fn stop(&self) {
+        let _ = self.command_sender.send(AudioCommand::Stop);
+    }
+
+    pub fn set_volume(&self, volume: f32) {
+        let _ = self.command_sender.send(AudioCommand::SetVolume(volume));
+    }
+
+    pub fn seek(&self, position: f64) {
+        let _ = self.command_sender.send(AudioCommand::Seek(position));
+    }
+
+    pub fn get_position(&self) -> f64 {
+        *self.current_position.lock().unwrap()
+    }
+
+    pub fn get_duration(&self) -> f64 {
+        *self.total_duration.lock().unwrap()
+    }
+
+    pub fn get_current_song(&self) -> Option<String> {
+        self.current_song.lock().unwrap().clone()
+    }
+
+    pub fn check_status(&self) -> Option<AudioStatus> {
+        self.status_receiver.try_recv().ok()
+    }
+}
+
+impl Drop for AudioEngine {
+    fn drop(&mut self) {
+        let _ = self.command_sender.send(AudioCommand::Quit);
+    }
+}
---
+++ src/playlist.rs
@@ -0,0 +1,168 @@
+use std::path::PathBuf;
+use walkdir::WalkDir;
+
+#[derive(Debug, Clone)]
+pub struct Song {
+    pub path: PathBuf,
+    pub title: String,
+    pub duration_secs: f64,
+    pub artist: Option<String>,
+    pub album: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Playlist {
+    pub name: String,
+    pub songs: Vec<Song>,
+    pub current_index: Option<usize>,
+    pub shuffle: bool,
+    pub repeat: bool,
+}
+
+impl Playlist {
+    pub fn new(name: &str) -> Self {
+        Playlist {
+            name: name.to_string(),
+            songs: Vec::new(),
+            current_index: None,
+            shuffle: false,
+            repeat: false,
+        }
+    }
+
+    pub fn add_song(&mut self, path: PathBuf) {
+        let title = path
+            .file_stem()
+            .map(|s| s.to_string_lossy().to_string())
+            .unwrap_or_else(|| "Unknown".to_string());
+
+        self.songs.push(Song {
+            path,
+            title,
+            duration_secs: 0.0,
+            artist: None,
+            album: None,
+        });
+    }
+
+    pub fn add_songs_from_dir(&mut self, dir: &PathBuf) {
+        let extensions = ["mp3", "wav", "flac", "ogg", "m4a", "aac", "wma"];
+
+        for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+            if entry.file_type().is_file() {
+                if let Some(ext) = entry.path().extension() {
+                    if extensions
+                        .iter()
+                        .any(|e| e == &ext.to_string_lossy().to_lowercase())
+                    {
+                        self.add_song(entry.path().to_path_buf());
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn remove_song(&mut self, index: usize) {
+        if index < self.songs.len() {
+            self.songs.remove(index);
+            if let Some(current) = self.current_index {
+                if current >= index {
+                    if current > 0 {
+                        self.current_index = Some(current - 1);
+                    } else {
+                        self.current_index = if self.songs.is_empty() {
+                            None
+                        } else {
+                            Some(0)
+                        };
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn get_current_song(&self) -> Option<&Song> {
+        self.current_index.and_then(|i| self.songs.get(i))
+    }
+
+    pub fn next(&mut self) -> Option<&Song> {
+        if self.songs.is_empty() {
+            return None;
+        }
+
+        match self.current_index {
+            Some(i) => {
+                if self.shuffle {
+                    use rand::Rng;
+                    let mut rng = rand::thread_rng();
+                    let new_index = rng.gen_range(0..self.songs.len());
+                    self.current_index = Some(new_index);
+                } else if i + 1 < self.songs.len() {
+                    self.current_index = Some(i + 1);
+                } else if self.repeat {
+                    self.current_index = Some(0);
+                } else {
+                    return None;
+                }
+            }
+            None => {
+                if !self.songs.is_empty() {
+                    self.current_index = Some(0);
+                }
+            }
+        }
+
+        self.get_current_song()
+    }
+
+    pub fn previous(&mut self) -> Option<&Song> {
+        if self.songs.is_empty() {
+            return None;
+        }
+
+        match self.current_index {
+            Some(i) => {
+                if i > 0 {
+                    self.current_index = Some(i - 1);
+                } else if self.repeat {
+                    self.current_index = Some(self.songs.len() - 1);
+                } else {
+                    return None;
+                }
+            }
+            None => {
+                if !self.songs.is_empty() {
+                    self.current_index = Some(0);
+                }
+            }
+        }
+
+        self.get_current_song()
+    }
+
+    pub fn play_at(&mut self, index: usize) -> Option<&Song> {
+        if index < self.songs.len() {
+            self.current_index = Some(index);
+            self.get_current_song()
+        } else {
+            None
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.songs.clear();
+        self.current_index = None;
+    }
+
+    pub fn total_duration(&self) -> f64 {
+        self.songs.iter().map(|s| s.duration_secs).sum()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.songs.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.songs.len()
+    }
+}
