dbf6c393bd5f74dc2b169861adfa9ab19c79c55f
praveensaummya <praveensaummya25@gmail.com>
2026-06-24 20:52:35 +0530

    clean up testfiles

--- scratch.rs
+++
@@ -1,6 +0,0 @@
-use rodio::{Sink, OutputStream};
-fn main() {
-    let (_stream, handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&handle).unwrap();
-    let _ = sink.try_seek(std::time::Duration::from_secs(10));
-}
